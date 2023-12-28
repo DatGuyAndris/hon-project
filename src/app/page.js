06a7ba164eed76e1e7d6cfc7300fb92884ad3294
DatGuyAndris <datguyandris@gmail.com>
2023-12-28 20:12:17 +0000

    edit

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     // Website stuff here
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      AAAAAAAAfasfsdfsadfs
+      AAAAAAAAfasfsdfsadfssadsadas2q4
     </main>
   )
 }
