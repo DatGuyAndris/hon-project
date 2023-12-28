93534e612449846f6e635f5c4cb1fd3d889bcf09
DatGuyAndris <datguyandris@gmail.com>
2023-12-28 20:09:26 +0000

    init commit

--- src/app/hon/page.js
+++ src/app/hon/page.js
@@ -1,14 +1,9 @@
 import Image from 'next/image'
 
 export default function Home() {
-
-//Code stuff here
-
-
   return (
-    // Website stuff here
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      AAAAAAAAfasfsdfsadfs
+      AAAAAAAA
     </main>
   )
 }
--- src/app/hon2/page.js
+++ src/app/hon2/page.js
@@ -1,12 +1,7 @@
 import Image from 'next/image'
 
 export default function Home() {
-
-//Code stuff here
-
-
   return (
-    // Website stuff here
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       AAAAAAAAfasfsdfsadfs
     </main>
