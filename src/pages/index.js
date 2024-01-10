08222a481f3ef667fe2eefadc7f9df4506efcb79
DatGuyAndris <datguyandris@gmail.com>
2024-01-10 13:36:44 +0000

    access token displaying

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,32 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useSession } from 'next-auth/react'
+import {useEffect, useState} from "react"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  // const{data:session} = useSession()
-  // console.log(session.user.accessToken)
+   const{data:session} = useSession()
+   const [x, setX] = useState('')
+   const [playlists, setPlayLists] = useState([])
+
+   useEffect(()=>{
+    
+    
+    if (session && session.accessToken) {
+      setX(session.accessToken) 
+  
+   }
+  },[session])
+   
+   //console.log(session.user.accessToken)
   return (
     <main
       className=""
     >
-      <div> aaaa</div>
+      <div> aaaa///  access token: {x}</div>
+      <div> Playlists: {playlists.map((playlist)=><div key={playlist.id}>{playlist.name}</div>)}</div>
       
     </main>
   )
